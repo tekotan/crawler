7c31f25421a88ffb7ad89151a03371011239bbcb
Tanish Baranwal <b.tanish@tugun.ai>
2020-07-16 10:11:25 -0700

    serial changes

--- raspberry_pi/mechanical_ops.py
+++ raspberry_pi/mechanical_ops.py
@@ -1,13 +1,12 @@
 import serial
 import time
 import threading
-ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=None)
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=None)
 
 def set_speed(action_list):
     ser.write(str.encode('A'))
     for i in action_list:
-        ser.write(chr(int(i)))
+        ser.write(str.encode(str(chr(int(i)))))
     time.sleep(0.7)
 
 def get_state():
@@ -17,3 +16,4 @@ def get_state():
     if state_list[12] > 1100:
         state_list[12] = 1100
     return state_list
+
